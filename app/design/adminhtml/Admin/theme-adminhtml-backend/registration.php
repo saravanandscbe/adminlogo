310b3a670ca19f1cea49a07924514b6845d959c4
Saravanan DS <saravanandscbe@gmail.com>
2019-03-14 14:27:02 +0530

    Add files via upload

---
+++ app/design/adminhtml/Admin/theme-adminhtml-backend/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2015 Magenest. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'adminhtml/Admin/theme-adminhtml-backend',
+    __DIR__
+);
